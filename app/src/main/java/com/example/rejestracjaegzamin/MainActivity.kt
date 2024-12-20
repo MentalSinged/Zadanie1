1ecd85e7d18d7045cb6242d0b9456598153dbbf6
SInged <114356860+MentalSinged@users.noreply.github.com>
2024-12-20 21:05:48 +0100

    Update Alpha 3.0.1
    Ukonczono Aplikacje Moblina

--- app/src/main/java/com/example/rejestracjaegzamin/MainActivity.kt
+++ app/src/main/java/com/example/rejestracjaegzamin/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.rejestracjaegzamin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val editTextEmail: EditText = findViewById(R.id.Email_EditText)
+        val editTextPassword: EditText = findViewById(R.id.Password_EditText)
+        val editTextRepeatPassword: EditText = findViewById(R.id.Repeat_Password_EditText)
+        val buttonZatwierdz: Button = findViewById(R.id.Zatwierdz_Button)
+        val showall: TextView = findViewById(R.id.ShowAll_TextView)
+
+
+
+
+        buttonZatwierdz.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password1 = editTextPassword.text.toString()
+            val password2 = editTextRepeatPassword.text.toString()
+            if (email.contains("@")){
+                showall.setText("Witaj! ${editTextEmail.getText()}")
+            }
+            else if (password1 != password2 || password1.isEmpty()){
+                showall.setText("hasla sie roznia")
+            }
+            else{
+                showall.setText("nieprawidlowy adress email")
+            }
+        }
+
+
     }
 }
