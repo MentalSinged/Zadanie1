6cf1703013516e856a302b80126e2a6a681066a4
SInged <114356860+MentalSinged@users.noreply.github.com>
2024-12-22 20:27:47 +0100

    Version 123312312312
    Ukonczono aplikacje konsolowa

--- APLIKACJA_KONSOLOWA/Aplikacja.py
+++ APLIKACJA_KONSOLOWA/Aplikacja.py
@@ -1,1 +1,34 @@
-class 
+class SelectionSorter:
+    def __init__(self):
+        self.array: list =[]
+
+    def sort(self):
+        for i in range(len(self.array)):
+            max_index = self.max_value(i,None)
+            self.array[i], self.array[max_index] = self.array[max_index], self.array[i]
+
+    def max_value(self,start,stop):
+        if start is None:
+            start = 0
+        if stop is None:
+            stop = len(self.array)
+
+        greatest_index = None
+        for i in range(start,stop):
+            if greatest_index is None or self.array[i] > self.array[greatest_index]:
+                greatest_index = i
+        return greatest_index
+
+if __name__ == "__main__":
+    sort = SelectionSorter()
+    for i in range(10):
+        array_value = int(input(f"Wprowadz wartosc: {i+1}: "))
+        sort.array.append(array_value)
+
+    print("Wprowadzona Tablica: ")
+    print(sort.array)
+
+    sort.sort()
+    print("Posortowana tablica: ")
+    print(sort.array)
+
